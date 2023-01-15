0a168db002fe4844a0fac419ac5885f8f6b1bd06
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2023-01-14 16:54:15 -0800

    Update Path.java

--- CS3310/Path.java
+++ CS3310/Path.java
@@ -4,28 +4,6 @@ import java.lang.Object;
 public class Path{
     public static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) {
-    //switches between graph and graph2 in floyd_Warshall method to test set graphs vs randomly generated graphs
-    /*
-    System.out.println("Enter n value: ");
-    Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
-    int[][] graph2 = new int[n][n];
-    Random random = new Random();
-    int num = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            num = random.nextInt(100);
-            if(i == j){
-                graph2[i][j] = 0;
-            }
-            else{
-                graph2[i][j] = num;
-            }
-            System.out.print(graph2[i][j] + "\t");
-        }
-        System.out.println();
-    }
-    */
     //set graph
     int[][] graph = {
         {0,3,INF,7},
@@ -34,15 +12,7 @@ public class Path{
         {2,INF,INF,0}
     };
     int[] dist = new int[graph.length];
-
-    //Time Measure
-    long startTime = System.nanoTime();
-
     dijkstra(graph, 0, dist);
-    
-    //Time End and Print
-    long endTime = System.nanoTime();
-    System.out.println(endTime - startTime);
 
     //Output
     System.out.println("Shortest distance from S = 0:");
